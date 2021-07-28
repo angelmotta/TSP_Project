ea53a2ce13550101353cbda36951fadf69c2477e
Piero Morales <piero.morales@utec.edu.pe>
2021-07-27 22:07:43 -0500

    Avance proceso maestro

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include <vector>
 #include <list>
+
 using namespace std;
+
+int nVertices = 0;
 
 int main (int argc, char *argv[]) {
     int rank, size;
@@ -12,12 +15,42 @@ int main (int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (rank == master) {
-        // TODO
         // Leer matriz adyacencia
+        cin >> nVertices;
+        vector <int> grafo(nVertices * nVertices, 0);
+        for (int i = 0; i < nVertices; i++) {
+            for (int j = 0; j < nVertices; j++) {
+                cin >> grafo[i * nVertices + j];
+            }
+        }
+
         // Broadcast del número de vertices y la matriz
+        MPI_Bcast(&nVertices, 1, MPI_INT, master, MPI_COMM_WORLD);
+        MPI_Bcast(&grafo[0], nVertices * nVertices, MPI_INT, master, MPI_COMM_WORLD);
+
         // Setear estado de procesos (ocupado o libre)
+        int procLibres = size - 1;
+        vector <bool> procOcupados(size, false);
+        procOcupados[master] = true;
+
+        // Obtener el primer proceso libre
+        int procDisponible = 1;
+        procLibres--;
+        procOcupados[procDisponible] = true;
+
         // Inicializar pathTSP y metada
-        // Enviar pathTSP y metadata
+        int costoOptimo = INT_MAX;
+        vector <int> camino(n, -1);
+
+        // metadata[0] = costo optimo | metadata[1] = costo actual
+        // metadata[2] = ultimo vertice del camino | metadata[3] = ha llegado a la hoja
+        vector <int> metadata(4, 0);
+        metadata[0] = costoOptimo;
+
+        // Enviar camino y metadata
+        MPI_Send(&camino[0], n, MPI_INT, procDisponible, 1, MPI_COMM_WORLD);
+        MPI_Send(&metadata[0], 4, MPI_INT, procDisponible, 1, MPI_COMM_WORLD);
+
         // Mientras haya algun proceso trabajando seguir computando el TSP
         // Detener procesos esclavos
         // Imprimir costo minimo
